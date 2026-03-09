31a5cca0aef3916fe6f9498b1e1048e0215fed5c
Nur Islam <mdnurislam15177@gmail.com>
2026-03-09 20:26:55 +0600

    Refactor: Refactor imageUploader function

--- src/utils/imageUploader.js
+++ src/utils/imageUploader.js
@@ -1,20 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const uploadImageToImgBB = async (file) => {
+export const imageUploader = async (file) => {
+  const MAX_SIZE = 200 * 1024; // 200KB
+
+  if (file && file.size > MAX_SIZE) {
+    throw new Error("Image size must be less than 200kb");
+  }
+
+  if (!file) return null;
+
   const formData = new FormData();
   formData.append("image", file);
 
   try {
     const response = await axios.post(
       `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
-      formData
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
     );
 
-    if (response.data.success) {
+    if (response.data?.success) {
       return response.data.data.url;
+    } else {
+      throw new Error("Upload failed from server side");
     }
   } catch (error) {
-    console.error("Upload error:", error);
-    throw new Error("Image upload failed");
+    console.error("ImgBB Error Details:", error.response?.data);
+    throw new Error(error.response?.data?.error?.message || "Image upload failed");
   }
 };
